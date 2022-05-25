b3728746e2c2033589d3d87cc6128023b018dc5e
Shawn Smith <sds.smith24@gmail.com>
2022-05-25 15:47:24 -0400

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/weather/'
   : '/'
-})
+}
